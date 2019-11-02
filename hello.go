a09bfbcad002ee90980c4021c75495a27aa6f519
Treighton Mauldin <treightonmauldin@Tmoney.local>
2019-11-02 15:45:53 -0700

    added 404 page and status

--- hello.go
+++ hello.go
@@ -13,6 +13,9 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to my nightmare</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "<h1>Contact</h1><a href=\"mailto:treighton@gmail.com\">Message me bitch</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>404<br />UM WTF GET OUTTA HERE</h1>")
 	}
 }
 
